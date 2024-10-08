9726a2eb7df499fe0b9eefbf10677dc714ced3da
Moritz Poßecker <M.Possecker@deskcenter.com>
2024-10-08 17:35:38 +0200

    return week dict instead of int

--- endpoints/current_week_getter.py
+++ endpoints/current_week_getter.py
@@ -3,9 +3,10 @@ import requests
 from utils.current_season_getter import get_current_season
 
 
-def get_current_week() -> int:
+def get_current_week() -> dict:
     url = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?limit=1&dates='
-    url = url + str(get_current_season())
+    current_season = get_current_season()
+    url = url + str(current_season)
     request = requests.get(url)
     data = request.json()
     calendar = data['leagues'][0]['calendar'][1]
@@ -15,6 +16,18 @@ def get_current_week() -> int:
         end_date = datetime.strptime(entry['endDate'], '%Y-%m-%dT%H:%MZ')
         now = datetime.now()
         if start_date <= now <= end_date:
-            return int(entry['value'])
+            return {
+                'Id': int(str(entry['value']) + str(current_season)),
+                'Number': int(entry['value']),
+                'Season': current_season,
+                'StartDate': start_date,
+                'EndDate': end_date,
+            }
 
-    return -1
+    return {
+        'Id': -1,
+        'Number': None,
+        'Season': None,
+        'StartDate': None,
+        'EndDate': None,
+    }
